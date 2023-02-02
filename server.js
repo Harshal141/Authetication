cfe8df956a09637dfbd19e67303a9d0752d42457
Harshal141 <harshalmukundapatil@gmail.com>
2023-02-02 23:10:30 +0530

    Express and ejs files basic setup.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Setting up the view engine
+app.set('view engine', 'ejs');
+
+// Setting up the static files
+app.use(express.static('public'));
+
+// helps us to get data from req.body
+app.use(express.json({}));
+app.use(express.urlencoded({ extended: false }));
+
+// Renderring the pages on requests
+app.get('/', (req, res) => res.render('index'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/register', (req, res) => res.render('register'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
